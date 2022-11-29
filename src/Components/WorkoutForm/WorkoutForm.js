913b933f27f25473cc543bf37e1b84adee3e83f5
Adil <adilk2001@outlook.com>
2022-11-28 16:14:34 -0800

    clearing input fields

--- src/Components/WorkoutForm/WorkoutForm.js
+++ src/Components/WorkoutForm/WorkoutForm.js
@@ -5,17 +5,17 @@ export default function WorkoutForm() {
     const activityInput = React.useRef();
     const quantityInput = React.useRef();
 
-
     const handleSubmit = (event) => {
         event.preventDefault();
-
 
         const activityValue = activityInput.current.value;
         const quantityValue = quantityInput.current.value;
 
         console.log(activityValue, quantityValue);
-    }
 
+        activityInput.current.value = '';
+        quantityInput.current.value = '';
+    }
 
     return (
         <form action="#" method="GET" onSubmit={handleSubmit}>
